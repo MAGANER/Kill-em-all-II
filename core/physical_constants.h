0b1ddbd3b6a57fa62c4917065299621feda935de
maganer.krik <maganer.krik@mail.ru>
2019-01-08 22:57:36 +0200

    added annotation

--- core/physical_constants.h
+++ core/physical_constants.h
@@ -1,3 +1,16 @@
 #pragma once
+/*
+these variables are used to scale 
+MKS(meter-kilogram-second) values
+to standart render values,such as
+pixels. 
+it is needed, because box2D uses MKS value system.
+
+
+
+example:
+a* SCALE = b pixels
+a/ SCALE = b meters
+*/
 const float SCALE = 30.0f;
 const float DEG = 57.29577f;

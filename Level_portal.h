004c1407c3c9f42ca61f11c709e8090095fb8ac2
maganer.krik <maganer.krik@mail.ru>
2019-01-09 20:29:11 +0200

    nothing changed

--- Level_portal.h
+++ Level_portal.h
@@ -8,11 +8,11 @@ public:
 	void react(void* obj_type, void* object,b2World* & world);
 
 	Level_portal(int half_width,
-		int half_height,
-		float32 density,
-		b2BodyType type,
-		b2World* & world,
-		void* user_data);
+		         int half_height,
+		         float32 density,
+		         b2BodyType type,
+		         b2World* & world,
+		         void* user_data);
 	~Level_portal();
 };
 

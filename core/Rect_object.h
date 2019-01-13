5de6ac3cb7e10537576f30dfe06c05d5e1c9afab
maganer.krik <maganer.krik@mail.ru>
2019-01-13 22:02:22 +0200

    overrided constructor

--- core/Rect_object.h
+++ core/Rect_object.h
@@ -2,16 +2,14 @@
 #include"Graphical_object.h"
 #include"Physical_object.h"
 
-#include<string>
-using namespace std;
-
 class Rect_object : public Graphical_object, public Physical_object
 {
 private:
 	b2PolygonShape shape;
 public:
-	Rect_object(int half_width, int half_height,int density,b2World* & world, string image, float x, float y);
-	Rect_object(int half_width, int half_height, int density,b2BodyType type ,b2World* & world, string image, float x, float y);
+	Rect_object(int half_width, int half_height, float32 density, b2BodyType type,b2World* & world);
+	Rect_object(int half_width, int half_height, float32 density,b2World* & world, string image, float x, float y);
+	Rect_object(int half_width, int half_height, float32 density,b2BodyType type ,b2World* & world, string image, float x, float y);
 	virtual ~Rect_object();
 };
 
